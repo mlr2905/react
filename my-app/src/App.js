33491adfd4292228ee0c85abaa10fba854174d2b
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-03-14 19:57:17 +0200

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react'
+import Car from './components/Car'
+class App extends Component {
+  render() {
+    // create component called HelloWolrd
+    // inside <h1>Hello world!<h1> in green
+    // line #10 <HelloWorld />
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <Car />
+        <Car />
+        <Car />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
